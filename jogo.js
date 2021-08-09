3d72b532f91679e90628248451c378bb7012d9df
Daniel Reis <danielreisp.2020@gmail.com>
2021-08-08 22:23:33 -0300

    Inserindo imagem e fazendo a mesma aparecer de forma randomica

--- jogo.js
+++ jogo.js
@@ -4,7 +4,23 @@ var largura = 0
 function ajustaTamanhoJogo(){
     altura = window.innerHeight
     largura =  window.innerWidth
-
-    console.log(altura,largura)
 }
 ajustaTamanhoJogo()
+
+function posicaoRandomica(){
+    var posicaox = Math.floor(Math.random() * largura) -90
+    var posicaoy = Math.floor(Math.random() * altura) - 90
+
+    posicaox = posicaox + posicaox < 0 ? 0 : posicaox
+    posicaoy = posicaoy + posicaoy < 0 ? 0 : posicaoy
+
+    //criando o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaox + 'px'
+    mosquito.style.top = posicaoy + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}

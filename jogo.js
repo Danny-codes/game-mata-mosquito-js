a470dbf22775f6a52215e7e1d68cc9281b4775c6
Daniel Reis <danielreisp.2020@gmail.com>
2021-08-09 21:08:42 -0300

    configurando cronometro e fluxo de vitoria

--- jogo.js
+++ jogo.js
@@ -1,12 +1,26 @@
 var altura= 0
 var largura = 0
 var vidas = 1
+var tempo = 10
 
 function ajustaTamanhoJogo(){
     altura = window.innerHeight
     largura =  window.innerWidth
 }
 ajustaTamanhoJogo()
+
+var cronometro = setInterval(function(){
+    tempo -= 1
+
+    if(tempo < 0){
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = 'vitoria.html'
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+   
+}, 1000)
 
 function posicaoRandomica(){
 
